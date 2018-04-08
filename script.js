25d9fa1194dfe49a0cfd6a03f5baff74b2755976
Sklinay <kirua29@yahoo.fr>
2018-04-08 06:43:58 +0200

    Nique ta race il est 6h43

---
+++ script.js
@@ -0,0 +1,131 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+$("#floorSlider").slider({
+    change: function (event, ui) {
+        console.log(event);
+    }
+});
+
+/*$('#rec1').draggable();*/
+  $( function() {
+    $( "#floorSlider" ).slider({min: 0, max: 1, value: 0, animate: true, change: switchFloor});
+  } );
+function switchFloor(event, ui) {
+    if (ui.value == 0)    
+        showRDC();    
+    else        
+        show1er();
+}
+
+var plan = $('#plan');
+var planImg = $('#planImg');
+
+/////////////////////////////////////////////////////////////////////RDC
+var eleRDC = [];
+//Porte garage
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 2%; height: 24%; top: 2.8%; left: 61.2%; background-color: grey;"
+}).hide());
+
+//Fenêtre garage
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 15%; top: 7%; left: 0%; background-color: grey;"
+}).hide());
+
+//Fenêtre pièce a gauche de l'escalier
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 10%; top: 35%; left: 0%; background-color: grey;"
+}).hide());
+
+//Fenêtre au milieu à gauche
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 15%; top: 57%; left: 0%; background-color: grey;"
+}).hide());
+
+//Fenêtre pièce en bas à gauche
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 10%; top: 84%; left: 0%; background-color: grey;"
+}).hide());
+
+//Fenêtre pièce en bas à Droite
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 10%; top: 84%; left: 98%; background-color: grey;"
+}).hide());
+
+//Porte pièce en haut à gauche
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 7%; top: 37%; left: 54%; background-color: grey;"
+}).hide());
+
+//Porte pièce en bas à gauche
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 7%; top: 85%; left: 54%; background-color: grey;"
+}).hide());
+
+//Porte pièce en bas à droite
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 7%; top: 85%; left: 69%; background-color: grey;"
+}).hide());
+
+//Porte entrée maison
+eleRDC.push($("<div>").attr({
+    'style': "position: absolute; width: 1.9%; height: 8%; top: 57%; left: 98%; background-color: grey;"
+}).hide());
+
+
+function loadRDC() {
+    eleRDC.forEach(function (e) {
+        e.appendTo(plan);
+    });
+}
+
+function hideRDC() {
+    eleRDC.forEach(function (e) {
+        e.hide();
+    });
+}
+
+function showRDC() {
+    planImg.siblings().hide();
+    planImg.attr("src", "planrdc.png");
+
+    eleRDC.forEach(function (e) {
+        e.show();
+    });
+     $('#floorLabel').html("Rez-de-chaussée");
+}
+loadRDC();
+showRDC();
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////1er étage
+var ele1er = [];
+
+function load1er() {
+    ele1er.forEach(function (e) {
+        e.appendTo(plan);
+    });
+}
+
+function hide1er() {
+    ele1er.forEach(function (e) {
+        e.hide();
+    });
+}
+
+function show1er() {
+    planImg.siblings().hide();
+    planImg.attr("src", "plan1er.png");
+    ele1er.forEach(function (e) {
+        e.show();
+    });
+     $('#floorLabel').html("1er étage");
+}
+load1er();
+/////////////////////////////////////////////////////////////////////
